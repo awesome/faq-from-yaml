9432f4c94f0bc47281f3989cf43b39cc940fc783
Scot Strube <scot.strube@gmail.com>
2013-04-29 13:20:45 -0700

    cleanup, more to do

--- faq_from_yaml.rb
+++ faq_from_yaml.rb
@@ -2,7 +2,15 @@
 # HTML FAQ pages.  In-line comments, links and email addresses use Markdown-like syntax.
 
 require 'YAML'
+verbose = true
+
 faq = YAML.load(File.read("example.yml"))
+if verbose
+  p "YAML loaded"
+#  p "Found #{faq.count} categories with an average of #{faq.inject{ |sum, el| sum + el["questions"].count }.to_f / faq.size} questions each."
+#  p "FYI: The following categories have only one question:"
+#  p "FYI: The following categories have no questions:"
+end
 
 f = File.open('example.html', 'w')
 old_out = $stdout
@@ -18,6 +26,10 @@ faq.each do |category|
     #put answers in <p></p>
     question["answer"] = "<p>#{question["answer"]}</p>"
     question["answer"].gsub!(/\n(.)/,'</p><p>\1')
+
+    #NOTE: The important thing below is to NOT find bracketed text that contains other bracketed text.  That
+    # would mean there are multiple links in the paragraph and we've grabbed the first bracket of the first one and
+    # the last bracket of the last one.
 
     #email links and link text
     question["answer"].gsub!(/\[([^\[\]]*)\][[:blank:]]*\[([^@\[\]]+@[^@\[\]]+)\]/,'<a href=\'mailto:\2\'>\1</a>')
@@ -50,11 +62,9 @@ end
 
 #BUILD QUESTIONS AND ANSWERS
 faq.each do |category|
-  #puts "<h2><a name='#{category["nickname"]}'>#{category["category"]}</a></h2>"
   puts "<h2 class='anchor' id='#{category["nickname"]}'>#{category["category"]}</h2>"
   puts "<p>#{category["description"]}</p>"
   category["questions"].each do |question|
-#    puts "<p><b><a name='#{question["nickname"]}'>#{question["question"]}</a></b></p>"
     puts "<p class='anchor' id='#{question["nickname"]}'><b>#{question["question"]}</b></p>"
     # the regex below prevents us from replacing the last newline
     puts "#{question["answer"]}"
